ce57780c78e36cf754e3fdb39cae9973aff1ebab
Austin Ameh <arewageek@gmail.com>
2025-01-30 13:02:50 +0100

    App basic structure

---
+++ app/Livewire/SingleTask.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SingleTask extends Component
+{
+    public function render()
+    {
+        return view('livewire.single-task');
+    }
+}
---
+++ resources/views/livewire/tasks-highlight.blade.php
@@ -0,0 +1,37 @@
+<div class="bg-white overflow-hidden shadow-sm rounded-3xl">
+    <div class="p-6 text-gray-900">
+        <div class="flex items-center justify-between">
+            <div class="flex flex-col gap-3">
+                <h1 class="text-3xl font-bold">Today's Tasks</h1>
+                <h4 class="text-xl font-medium text-gray-600/70">
+                    Thursday Jan 30th, 2023-02-23
+                </h4>
+            </div>
+            <div>
+                <button class="bg-sky-100 px-4 py-2 rounded-lg text-sky-800 text-lg font-bold shadow-sm hover:shadow-lg transition hover:scale-105">
+                    + New Task
+                </button>
+            </div>
+        </div>
+
+        <div class="my-5 flex items-center gap-3 divide-x-2 text-gray-500">
+            <div class="text-sm font-bold text-sky-800 flex items-center justify-center gap-x-3">
+                All Tasks
+                <livewire:tab-count count="10" shade="sky" isActive={{true}} />
+            </div>
+            <div class="pl-2 flex items-center justify-center gap-x-3">
+                Active Tasks
+                <livewire:tab-count count="0" shade="gray" />
+            </div>
+            <div class="pl-2 flex items-center justify-center gap-x-3">
+                Completed
+                <livewire:tab-count count="0" shade="gray" />
+            </div>
+            <div class="pl-2 flex items-center justify-center gap-x-3">
+                Canceled
+                <livewire:tab-count count="0" shade="gray" />
+            </div>
+        </div>
+
+    </div>
+</div>
